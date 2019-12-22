53ba0be2d35c1cdd7c015ae59eee342c0a7fbd23
Danielle Adams <danielle.adams@heroku.com>
2019-12-22 08:02:32 -0500

    add more logging

--- server.js
+++ server.js
@@ -5,31 +5,42 @@ const THREADS = 100;
 const PORT = process.env.PORT || 8000;
 
 let worker, threadCount;
+let activeThreads = 0, totalThreads = 0;
 let server = require('http');
 
-function startThreads(threads = THREADS) {
+function startThreads(threads) {
+  threads = Number(threads);
   for (let i = 1; i <= threads; i = add(i, 1)) {
     worker = new Worker("./tick.js", { workerData: i });
+    totalThreads = add(totalThreads, 1);
+    activeThreads = add(totalThreads, 1);
 
     worker.on('message', ({ id, date }) => {
       threadCount = add(threadCount, 1);
+      activeThreads--;
 
       if (threadCount === threads)
-      console.log(`Thread ${id} ends: ${date}`);
+        console.log(`Thread ${id} ends: ${date}`);
     });
   }
 }
 
-server = server.createServer((req, res) => {
-  threadCount = 0;
+function startServer(server) {
+  return server.createServer((req, res) => {
+    threadCount = 0;
 
-  req.on('data', (chunk) => {
-    let threads = JSON.parse(chunk).threads;
-    startThreads(threads);
+    req.on('data', (chunk) => {
+      let threads = JSON.parse(chunk).threads;
+      startThreads(threads);
+    });
+
+    console.log(`Total Threads Active: ${activeThreads}`)
+    console.log(`Total Threads Created: ${totalThreads}`);
+    res.end("OK");
   });
+}
 
-  res.end("OK");
-});
+server = startServer(server);
 
 server.on('clientError', (_, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
